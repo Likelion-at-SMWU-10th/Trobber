4e7cd692a8987850eb705266dbf3f33aa2aac2b7
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-11-17 20:25:12 +0900

    beijing,london

--- trobber_fe/src/pages/components/Countries.jsx
+++ trobber_fe/src/pages/components/Countries.jsx
@@ -5,18 +5,23 @@ import './Countries.css';
 function Countries() {
   return (
     <Carousel>
+      
+      {/* 일본 */}
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="img/japan.png"
-          alt="Japan"
+          src="img/tokyo.png"
+          alt="Tokyo"
         />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
+
+      {/* 파리 */}
       <Carousel.Item>
+      
         <img
           className="d-block w-100"
           src="img/paris.png"
@@ -28,6 +33,8 @@ function Countries() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
+
+      {/* 뉴욕 */}
       <Carousel.Item>
         <img
           className="d-block w-100"
@@ -41,7 +48,36 @@ function Countries() {
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
+      
       </Carousel.Item>
+       {/* 베이징 */}
+       <Carousel.Item>
+      
+      <img
+        className="d-block w-100"
+        src="img/beijing.png"
+        alt="Beijing"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+     {/* 런던 */}
+     <Carousel.Item>
+      
+      <img
+        className="d-block w-100"
+        src="img/london.png"
+        alt="London"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
     </Carousel>
   );
 }
