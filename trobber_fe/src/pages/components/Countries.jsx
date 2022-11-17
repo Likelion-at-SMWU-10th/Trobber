a94275d1e72f826cea57725ee27fa149bc2d41cc
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-11-17 20:06:57 +0900

    paris

--- trobber_fe/src/pages/components/Countries.jsx
+++ trobber_fe/src/pages/components/Countries.jsx
@@ -1,5 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Countries.css';
 
 function Countries() {
   return (
@@ -18,8 +19,8 @@ function Countries() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
+          src="img/paris.png"
+          alt="Paris"
         />
 
         <Carousel.Caption>
