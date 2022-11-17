ca69977429bb1d406ab008f758d5b2052718198a
nneo <aisapple1030@naver.com>
2022-11-18 00:10:34 +0900

    Home HMTL and CSS

--- trobber_fe/src/pages/Home.jsx
+++ trobber_fe/src/pages/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
+import styles from "../css/home.module.css";
 
 const Home = () => {
-    return (
-        <div>
-            <>
-            <h1>홈 화면입니다! (은비 작업)</h1>
-            </>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <img className={styles.logo} src="/images/logo.png" alt="logo" />
+      <div className={styles.buttons}>
+        <button className={styles.button}>Start with Google</button>
+        <button className={styles.button}>Start with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
