20d5717089e13b2e19ca52292de2cf729ab38b08
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-11-17 21:11:13 +0900

    memo

---
+++ trobber_fe/src/pages/components/Memo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Memo.css';
+
+const Memo = () => {
+    return (
+        <div className='memo'>
+            <p>Memo :</p>
+            <div className='input'>
+            <input></input>
+            </div>
+        </div>
+    );
+};
+
+export default Memo;
