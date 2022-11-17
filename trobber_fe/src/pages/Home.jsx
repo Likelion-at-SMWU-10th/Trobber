de838c8e720e217b17d0bf634c19a2f30b20e84b
nneo <aisapple1030@naver.com>
2022-11-18 00:33:11 +0900

    Home link buttons to mainpage

--- trobber_fe/src/pages/Home.jsx
+++ trobber_fe/src/pages/Home.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import styles from "../css/home.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate("/setting");
+  };
+  const onLogin = (event) => {
+    goToMain();
+  };
   return (
     <div className={styles.container}>
       <div className={styles.contents}>
         <img className={styles.logo} src="/images/logo.png" alt="logo" />
       </div>
       <div className={styles.buttons}>
-        <button className={styles.button}>Start with Google</button>
-        <button className={styles.button}>Start with Github</button>
+        <button className={styles.button} onClick={onLogin}>
+          Start with Google
+        </button>
+        <button className={styles.button} onClick={onLogin}>
+          Start with Github
+        </button>
       </div>
     </div>
   );
