18c44f7a4cd2c573213748237445205fd76409ce
nneo <aisapple1030@naver.com>
2022-11-20 00:17:45 +0900

    Carousel  및 문구 수정 완료

--- trobber_fe/src/pages/components/Countries.jsx
+++ trobber_fe/src/pages/components/Countries.jsx
@@ -1,5 +1,6 @@
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "../../css/countries.module.css";
 import "./Countries.css";
 
 function Countries() {
@@ -7,23 +8,93 @@ function Countries() {
     <Carousel indicators={false} interval={null}>
       {/* 일본 */}
       <Carousel.Item>
-        <img className="resizing" src="img/tokyo.png" alt="Tokyo" />
+        <div>
+          <div className={styles.upperInfo}>
+            <span className={styles.weather}>Raining</span>
+            <span className={styles.region}>Tokyo</span>
+          </div>
+          <img
+            className={styles.countryImage}
+            src="/images/JAPAN.png"
+            alt="Tokyo"
+          />
+          <div className={styles.bottomInfo}>
+            <span className={styles.date}>Dec. 20</span>
+            <span className={styles.time}>23:52</span>
+          </div>
+        </div>
       </Carousel.Item>
       {/* 파리 */}
       <Carousel.Item>
-        <img className="resizing" src="img/paris.png" alt="Paris" />
+        <div>
+          <div className={styles.upperInfo}>
+            <span className={styles.weather}>Cloudy</span>
+            <span className={styles.region}>Paris</span>
+          </div>
+          <img
+            className={styles.countryImage}
+            src="/images/PARIS.png"
+            alt="Paris"
+          />
+          <div className={styles.bottomInfo}>
+            <span className={styles.date}>Nov. 20</span>
+            <span className={styles.time}>12:15</span>
+          </div>
+        </div>
       </Carousel.Item>
       {/* 뉴욕 */}
       <Carousel.Item>
-        <img className="resizing" src="img/newyork.png" alt="New York" />
+        <div>
+          <div className={styles.upperInfo}>
+            <span className={styles.weather}>Sunny</span>
+            <span className={styles.region}>New York</span>
+          </div>
+          <img
+            className={styles.countryImage}
+            src="/images/NEWYORK.png"
+            alt="New York"
+          />
+          <div className={styles.bottomInfo}>
+            <span className={styles.date}>Oct. 30</span>
+            <span className={styles.time}>22:31</span>
+          </div>
+        </div>
       </Carousel.Item>
       {/* 베이징 */}
       <Carousel.Item>
-        <img className="resizing" src="img/beijing.png" alt="Beijing" />
+        <div>
+          <div className={styles.upperInfo}>
+            <span className={styles.weather}>Foggy</span>
+            <span className={styles.region}>Beijing</span>
+          </div>
+          <img
+            className={styles.countryImage}
+            src="/images/CHINA.png"
+            alt="Beijing"
+          />
+          <div className={styles.bottomInfo}>
+            <span className={styles.date}>Dec. 13</span>
+            <span className={styles.time}>09:12</span>
+          </div>
+        </div>
       </Carousel.Item>
       {/* 런던 */}
       <Carousel.Item>
-        <img className="resizing" src="img/london.png" alt="London" />
+        <div>
+          <div className={styles.upperInfo}>
+            <span className={styles.weather}>Raining</span>
+            <span className={styles.region}>London</span>
+          </div>
+          <img
+            className={styles.countryImage}
+            src="/images/LONDON.png"
+            alt="London"
+          />
+          <div className={styles.bottomInfo}>
+            <span className={styles.date}>Apr. 01</span>
+            <span className={styles.time}>10:32</span>
+          </div>
+        </div>
       </Carousel.Item>
     </Carousel>
   );
