8cfd41c50dc8cc2d925423d5e9acbe85d38ec5e5
nneo <aisapple1030@naver.com>
2022-11-19 23:30:54 +0900

    TimerSetting page Carousel 자동 슬라이드 및 버튼 수정

--- trobber_fe/src/pages/components/Countries.jsx
+++ trobber_fe/src/pages/components/Countries.jsx
@@ -1,83 +1,30 @@
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Countries.css';
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Countries.css";
 
 function Countries() {
   return (
-    <Carousel>
-      
+    <Carousel indicators={false} interval={null}>
       {/* 일본 */}
       <Carousel.Item>
-        <img
-          className="resizing"
-          src="img/tokyo.png"
-          alt="Tokyo"
-        />
-        <Carousel.Caption>
-          <h3>&nbsp;</h3>
-          <p>&nbsp;</p>
-        </Carousel.Caption>
+        <img className="resizing" src="img/tokyo.png" alt="Tokyo" />
       </Carousel.Item>
-
       {/* 파리 */}
       <Carousel.Item>
-      
-        <img
-          className="resizing"
-          src="img/paris.png"
-          alt="Paris"
-        />
-
-        <Carousel.Caption>
-          <h3>&nbsp;</h3>
-          <p>&nbsp;.</p>
-        </Carousel.Caption>
+        <img className="resizing" src="img/paris.png" alt="Paris" />
       </Carousel.Item>
-
       {/* 뉴욕 */}
       <Carousel.Item>
-        <img
-          className="resizing"
-          src="img/newyork.png"
-          alt="New York"
-        />
-
-        <Carousel.Caption>
-          <h3>&nbsp;</h3>
-          <p>
-          &nbsp;
-          </p>
-        </Carousel.Caption>
-      
+        <img className="resizing" src="img/newyork.png" alt="New York" />
       </Carousel.Item>
-       {/* 베이징 */}
-       <Carousel.Item>
-      
-      <img
-        className="resizing"
-        src="img/beijing.png"
-        alt="Beijing"
-      />
-
-      <Carousel.Caption>
-        <h3>&nbsp;</h3>
-        <p>&nbsp;</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-     {/* 런던 */}
-     <Carousel.Item>
-      
-      <img
-        className="resizing"
-        src="img/london.png"
-        alt="London"
-      />
-
-      <Carousel.Caption>
-        <h3>&nbsp;</h3>
-        <p>&nbsp;</p>
-      </Carousel.Caption>
-    </Carousel.Item>
+      {/* 베이징 */}
+      <Carousel.Item>
+        <img className="resizing" src="img/beijing.png" alt="Beijing" />
+      </Carousel.Item>
+      {/* 런던 */}
+      <Carousel.Item>
+        <img className="resizing" src="img/london.png" alt="London" />
+      </Carousel.Item>
     </Carousel>
   );
 }
